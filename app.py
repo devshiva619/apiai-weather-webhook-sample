64c87e21a059cb2d978c2e55485220997064f353
Devesh Verma <deveshverma619@gmail.com>
2018-02-20 15:32:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-#!/usr/bin/env python
+#from src import app
+# from flask import jsonify
 
-import urllib
+from __future__ import print_function
+from future.standard_library import install_aliases
+install_aliases()
+
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+
 import json
 import os
-import sys
 
 from flask import Flask
 from flask import request
@@ -19,94 +26,69 @@ def webhook():
 
     print("Request:")
     print(json.dumps(req, indent=4))
-    
+
     res = processRequest(req)
-    print("Request processed")
-    
+
     res = json.dumps(res, indent=4)
-    print(res)
-    
+    # print(res)
     r = make_response(res)
-    print(r)
-    
     r.headers['Content-Type'] = 'application/json'
     return r
 
 
 def processRequest(req):
-    if req.get("result").get("action") != "createCheckBook":
-        return {}
-    baseurl = "https://ldcif6u.wdf.sap.corp:44304/sap/opu/odata/sap/ZIVRC_SRV/WorkItems("
-    print(baseurl)
-    
+    if req.get("result").get("action") != "yahooWeatherForecast":
+       return None
+    baseurl = "https://query.yahooapis.com/v1/public/yql?"
     yql_query = makeYqlQuery(req)
-    print("yql_query1:")
-    print(yql_query)
-    
     if yql_query is None:
-        return {}
-    yql_url = baseurl + urllib.urlencode({yql_query}) + "?$format=json"
-    print(yql_query)
-    
-    result = urllib.urlopen(baseurl).read()
-    print(result)
+       return None
+    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+    result = urlopen(yql_url).read()
     data = json.loads(result)
-    print("data1:")
-    print(data)
-    
     res = makeWebhookResult(data)
     return res
 
 
 def makeYqlQuery(req):
     result = req.get("result")
-    print("result1:")
-    print(result)
     parameters = result.get("parameters")
-    print("parameters1:")
-    print(parameters)
-    city = parameters.get("workitemtype")
-    print("City1:")
-    print(city)
+    city = parameters.get("geo-city")
     if city is None:
         return None
-        
-        return "guid'" + "0005EEE4-48CC-1ED5-B0C9-FA163EA701AC" + "')"
-   #return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+
+    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "') and u='c'"
 
 
 def makeWebhookResult(data):
-    print("MakeWebhook method")
-    query = data.get('d')
-    print("Query1:")
-    print(query)
+    query = data.get('query')
     if query is None:
-        return {}
+        return None
 
-    result = query.get('WORKITEM_ID')
+    result = query.get('results')
     if result is None:
-        return {}
+        return None
 
-    channel = query.get('DESCRIPTION')
+    channel = result.get('channel')
     if channel is None:
+        return None
+
+    item = channel.get('item')
+    location = channel.get('location')
+    units = channel.get('units')
+    if (location is None) or (item is None) or (units is None):
+        return None
+
+    condition = item.get('condition')
+    if condition is None:
         return {}
-
-   # item = channel.get('item')
-   # location = channel.get('location')
-   # units = channel.get('units')
-   # if (location is None) or (item is None) or (units is None):
-   #    return {}
-
-   # condition = item.get('condition')
-   # if condition is None:
-   #     return {}
 
     # print(json.dumps(item, indent=4))
 
-   # speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-   #         ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
-	speech = " The Work Item No. " + result + " has been created for " + channel
-    print("Response1:")
+    speech = "Today the weather in " + location.get('city') + ": " + condition.get('text') + \
+             ", And the temperature is " + condition.get('temp') + " " + units.get('temperature')
+
+    print("Response:")
     print(speech)
 
     return {
@@ -116,6 +98,8 @@ def makeWebhookResult(data):
         # "contextOut": [],
         "source": "apiai-weather-webhook-sample"
     }
+
+
 
 
 if __name__ == '__main__':
